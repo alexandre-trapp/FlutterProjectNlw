ae302af0ab51603eac62b60633fcb8aa8285c853
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-06-21 21:55:44 -0300

    projeto inicial contador com flutter / UI com material design

---
+++ counter_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    ),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Meu primeiro App")
+      ),
+      body: Container(
+        color: Colors.purple,
+        child: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.green,
+          )
+        )
+      )
+    )
+  ));
+}
