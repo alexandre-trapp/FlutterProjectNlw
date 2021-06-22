4faca916e4effd8d321d894c70917a136a084957
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-06-22 07:48:39 -0300

    criado contador com flutter

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -2,23 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(
-      primarySwatch: Colors.purple,
-    ),
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Meu primeiro App")
-      ),
-      body: Container(
-        color: Colors.purple,
-        child: Center(
-          child: Container(
-            height: 200,
-            width: 200,
-            color: Colors.green,
-          )
-        )
-      )
-    )
-  ));
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Meu primeiro App")),
+        body: Center(
+            child: Text(
+          "CONTADOR\n$count",
+          textAlign: TextAlign.center,
+        )),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            increment();
+          },
+        ));
+  }
 }
